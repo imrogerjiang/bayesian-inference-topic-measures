5ecca5142e0f41064f00a1ce57255a6827662554
Roger Jiang <imrogerjiang@gmail.com>
2022-07-18 15:35:21 +1000

    Added plot_prior_postrr function

--- modeltools.py
+++ modeltools.py
@@ -2,7 +2,60 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import arviz as az
 
-def mcmc_diagnostics2(trace, summary_stat):
+def plot_sample_sim(prior_pred, postrr_pred, data, target="s"):
+    # Prior
+    # Plotting distribution of scores simulated from prior
+    prior_samples = prior_pred.prior_predictive.sel(chain=0)
+    postrr_samples = postrr_pred.posterior_predictive.sel(chain=0)
+
+    # Manipulating samples to plot
+    df_prior, na_s = downcast_df(prior_samples[target].to_dataframe().reset_index())
+    df_postrr, na_s = downcast_df(postrr_samples[target].to_dataframe().reset_index())
+
+    # Joining rater/topic/model/corpus information
+    data1 = data.copy()
+    data1[f"{target}_dim_0"] = data1.index
+    df_prior1 = pd.merge(df_prior, data1, on=target+"_dim_0", how="left")
+    df_postrr1 = pd.merge(df_postrr, data1, on=target+"_dim_0", how="left")
+
+    # Aggregating "1s" and counts by topic
+    prior_agg = df_prior1.groupby(["draw", "topic_id"]).agg({target:["sum", "count"]}).reset_index()
+    postrr_agg = df_postrr1.groupby(["draw", "topic_id"]).agg({target:["sum", "count"]}).reset_index()
+    obs_agg = df_prior1[df_prior1["chain"]==0].groupby(["draw", "topic_id"]).agg({"intrusion":["sum", "count"]}).reset_index()
+
+    # Calculating topic probabilities from sums and counts
+    prior_topic_prob = prior_agg[("s","sum")]/prior_agg[("s","count")]
+    postrr_topic_prob = postrr_agg[("s","sum")]/postrr_agg[("s","count")]
+    obs_topic_prob = obs_agg[("intrusion","sum")]/obs_agg[("intrusion","count")]
+
+    # Setting up plot
+    plot, ax = plt.subplots(1, 1,figsize=(10,6))
+    ax = az.plot_kde(np.array(prior_topic_prob), bw=0.05)
+    ax.get_lines()[0].set_color("orange")
+    ax.get_lines()[0].set_linestyle("--")
+    ax = az.plot_kde(np.array(postrr_topic_prob), bw=0.05)
+    ax.get_lines()[1].set_color("green")
+    ax.get_lines()[1].set_linestyle("--")
+    ax = az.plot_kde(np.array(obs_topic_prob), bw=0.05)
+    ax.set_title("Prior dist: number of 1's per topic")
+    ax.set_ylabel("Density")
+    ax.set_xlabel("p")
+    ax.legend(ax.get_lines(), ["Prior", "Posterior", "Observed"])
+
+    plt.plot()
+
+def create_summary_stat(trace):
+    summary_stat = az.summary(trace, round_to=4).reset_index()
+
+    # Creating parameter and parameter number columns
+    summary_stat["param"] = summary_stat["index"].str.split("[").str[0]
+    summary_stat["param_num"] = summary_stat["index"].str.split("[").str[1].str[:-1]
+    summary_stat["param"] = summary_stat["param"].astype("category")
+    summary_stat["param_num"] = summary_stat["param_num"].astype("category")
+    return summary_stat[["param", "param_num"]+list(summary_stat.columns[1:-2])]
+    
+
+def mcmc_diagnostics(trace, summary_stat):
     # Selecting parameters with highest rhat and lowest ess
     highest_rhat = summary_stat.sort_values(by="r_hat", ascending=False).head(1)
     lowest_ess = summary_stat.sort_values(by="ess_bulk", ascending=True).head(1)
